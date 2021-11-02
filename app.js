f117f49ee8a5cfc2d58da96282294e73d1f1aefa
zivserphos <zivfromisrael@gmail.com>
2021-11-02 17:02:11 +0200

    added body parser

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const {urlencoded} = require("body-parser");
 const app = express();
 
 app.use(cors());
+
+// body-parser
+app.use(express , urlencoded({extended: true}))
+app.use(express.json())
 
 app.use("/public", express.static(`./public`));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.get("/" , (req ,res) => {
+  res.send("zibi bibi")
+})
 
 module.exports = app;
