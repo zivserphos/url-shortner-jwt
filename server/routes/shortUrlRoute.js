60536e63d96f12942d9a95d6f08a95b36cb45872
zivserphos <zivfromisrael@gmail.com>
2021-11-05 14:03:10 +0200

    development

--- server/routes/shortUrlRoute.js
+++ server/routes/shortUrlRoute.js
@@ -7,13 +7,16 @@ const { isURL } = require("validator");
 
 shortUrlRouter.post("/", async (req, res, next) => {
   try {
+    if (req.body.originUrl.length > 200) {
+      throw "url is too long";
+    }
     if (isURL(req.body.originUrl)) {
       return res.send(await dataBase.addObjToDb(req.body.originUrl));
     } else {
       throw "INVALID URL";
     }
   } catch (err) {
-    if (err === "INVALID URL") {
+    if (err === "INVALID URL" || err === "url is too long") {
       return next({ status: 400, message: { error: err } });
     }
     return next({ message: { error: err } });
