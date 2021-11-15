7e7009e241933e6eff822b800b1c84d18a99106d
zivserphos <zivfromisrael@gmail.com>
2021-11-15 20:37:27 +0200

    fixed all due to eslint

--- server/app.js
+++ server/app.js
@@ -1,32 +1,33 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const { urlencoded } = require("body-parser");
-const shortUrlRouter = require("./routes/shortUrlRoute");
-const loginRouter = require("./routes/login")
-const statsRoute = require("./routes/statsRoute");
-const errorHandler = require("./handlers/errorHandler");
-const reDirectRouter = require("./routes/reDircetRouter");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { urlencoded } = require('body-parser');
+const path = require('path');
+const shortUrlRouter = require('./routes/shortUrlRoute');
+const loginRouter = require('./routes/login');
+const statsRoute = require('./routes/statsRoute');
+const errorHandler = require('./handlers/errorHandler');
+const reDirectRouter = require('./routes/reDircetRouter');
+
 const app = express();
-const path = require("path");
 
 app.use(cors());
 // body-parser
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/app", express.static(path.resolve(`./server/dist`)));
+app.use('/app', express.static(path.resolve(`./server/dist`)));
 
-app.use("/login" , loginRouter)
-app.use("/", reDirectRouter);
-//app.use(express.static(path.resolve(`./dist`)));
+app.use('/login', loginRouter);
+app.use('/', reDirectRouter);
+// app.use(express.static(path.resolve(`./dist`)));
 
-app.use("/api/shorturl/", shortUrlRouter);
-app.use("/api/statistic", statsRoute);
+app.use('/api/shorturl/', shortUrlRouter);
+app.use('/api/statistic', statsRoute);
 app.use(errorHandler);
 
-app.get("/app", (req, res) => {
-  res.sendFile(__dirname + "/dist/index.html");
+app.get('/app', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
 });
 
 module.exports = app;
